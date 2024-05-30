5badd18a93b1d7b9aaada2934ccc583dda457820
hossein <68683629+hossseinnorian@users.noreply.github.com>
2024-05-30 22:52:56 +0330

    Add files via upload

---
+++ ethereum.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+RPC_URL="https://g.w.lavanet.xyz:443/gateway/eth/rpc-http/44d5a08b6b538b9a4aa23e70976bf655"
+
+OUTPUT_DIR="logs"
+
+eth_data=$(curl -s -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_gasPrice","id":1}' "$RPC_URL")
+
+if [ $? -eq 0 ]; then
+    if [ ! -d "$OUTPUT_DIR" ]; then
+        mkdir "$OUTPUT_DIR"
+    fi
+    
+    echo "$eth_data" >> "$OUTPUT_DIR/ethereum.txt"
+    
+    echo "Ethereum data fetched and saved to $OUTPUT_DIR/ethereum.txt."
+else
+    echo "Failed to fetch Ethereum data."
+fi
---
+++ starknet.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+RPC_URL="https://g.w.lavanet.xyz:443/gateway/strk/rpc-http/44d5a08b6b538b9a4aa23e70976bf655"
+
+OUTPUT_DIR="logs"
+
+starknet_data=$(curl -s -H "Content-Type: application/json" -X POST --data '{"jsonrpc":"2.0","id":"1","method":"starknet_blockNumber"}' "$RPC_URL")
+
+if [ $? -eq 0 ]; then
+    if [ ! -d "$OUTPUT_DIR" ]; then
+        mkdir "$OUTPUT_DIR"
+    fi
+    
+    echo "$starknet_data" >> "$OUTPUT_DIR/starknet.txt"
+    
+    echo "StarkNet data fetched and saved to $OUTPUT_DIR/starknet.txt."
+else
+    echo "Failed to fetch StarkNet data."
+fi
